1e120060a79566ae880fa505a15d8f3100f5c006
JamesWithCode <minseok.jeong.james@gmail.com>
2022-11-02 11:57:14 +0900

    initial commit

---
+++ junit/src/test/java/com/jameswithcode/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameswithcode.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
