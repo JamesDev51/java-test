42e5d6d5c56c3edab0d4725dea21c324463c7b08
JamesWithCode <minseok.jeong.james@gmail.com>
2023-06-01 14:41:09 +0900

    230601 백기선 더 자바 테스트 프로젝트 생성

---
+++ whiteship-java-test/src/main/java/com/whiteshipjavatest/WhiteshipJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.whiteshipjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteshipJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteshipJavaTestApplication.class, args);
+	}
+
+}
